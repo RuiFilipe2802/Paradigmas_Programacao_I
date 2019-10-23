0ab4b447a01a4b5839f84d812b5ad1766fd620bc
ColdZora <56730224+ColdZora@users.noreply.github.com>
2019-10-23 17:24:09 +0100

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Client here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Client
+{
+   private Id id;
+   private Coordenates xy;
+   
+   
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String[] args){
+        Log start = new log();
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("----------------------------------");
+        System.out.println("----------EntregasExpress---------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------- 1 - Log In -----------");
+        System.out.println("----------- 2 - Registo ----------");
+        System.out.println("----------------------------------");
+    }
+}
